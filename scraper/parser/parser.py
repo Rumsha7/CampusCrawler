ac8b2769c22bb0bea88379841741067f49c53a07
Rumsha <rumsha7@gmail.com>
2018-02-20 18:32:21 -0500

    Updated parser

--- scraper/parser/parser.py
+++ scraper/parser/parser.py
@@ -1,1 +1,21 @@
-## Parses data from schedule into data structures
+import json
+''' Parses data from schedule into data structures '''
+
+# read file
+file_object = open("data_mcmaster", "r")
+
+# store it in data variable
+data = file_object.read()
+
+# parse json data
+data_dic = json.loads(data)
+
+courses = data_dic["courses"]
+philos = courses["PHILOS"]
+name = philos[0]["name"]
+
+print(name)
+
+file_object.close()
+
+print("Finished")
