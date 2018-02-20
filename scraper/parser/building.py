d19b5065fe055601a7c0fdc89f88c0744259a2a6
UMME SALMA Gadriwala <umme.salma.gadriwala@ibm.com>
2018-02-20 15:29:00 -0500

    Room

--- scraper/parser/building.py
+++ scraper/parser/building.py
@@ -11,36 +11,32 @@ class Building(object):
 	"""docstring for Building"""
 	bID = None #string
 	bName = None #string
-	bRooms = []
+	bRooms = set()
 	def __init__(self, id, name):
 		self.bID = id
 		self.bName = name
 
 	def addRoom(self, room):
 		print room
-		self.bRooms += [room]
+		self.bRooms.add(room)
 		return True
 
 	def removeRoom(self, room):
-		rIndex = self.findRoom(room)
-		if rIndex == -1: # room does not exist
+		if room in self.bRooms: # room does not exist
 			return False
-		self.bRooms.remove(rIndex)
+		self.bRooms.remove(room)
 		return True
 
 
 	def findRoom(self, room):
-		for i in range(len(self.bRooms)):
-			if self.bRooms[i] == room:
-				return i
-		return -1
+		return room in self.bRooms
 
 	def __str__(self):
-		return self.bID + ", " + self.bName + "\nRooms: " + str(self.bRooms)
+		return self.bID + ", " + self.bName + "\n Rooms: " + str(self.bRooms)
 
-b = Building("HH", "Hamilton Hall")
-b.addRoom("HH 109")
-b.addRoom("HH 107")
-print(b.findRoom("HH 108"))
-b.removeRoom("HH 107")
-print b
+#b = Building("HH", "Hamilton Hall")
+#b.addRoom("HH 109")
+#b.addRoom("HH 107")
+#print(b.findRoom("HH 109"))
+#b.removeRoom("HH 107")
+#print b
