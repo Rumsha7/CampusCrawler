834e2d7233c866e3b60da1c82e98aca655c1c65a
Rumsha <rumsha7@gmail.com>
2018-02-20 16:08:07 -0500

    rough draft

--- scraper/manipulator.py
+++ scraper/manipulator.py
@@ -1,16 +1,21 @@
 import mechanize
 
 br = mechanize.Browser()
-br.set_all_readonly(False)    # allow everything to be written to
-br.set_handle_robots(False)   # ignore robots
-br.set_handle_refresh(False)  # can sometimes hang without this
-#br.addheaders = 	      	# [('User-agent', 'Firefox')]
-
-response = br.open("https://applicants.mcmaster.ca/psp/prepprd/EMPLOYEE/PSFT_LS/c/COMMUNITY_ACCESS.CLASS_SEARCH.GBL")
-print(response.read())      # the text of the page
+response = br.open("https://www.timetablegenerator.io/")
+#print(response.read())      # the text of the page
 response1 = br.response()  # get the response again
-print(response1.read())     # can apply lxml.html.fromstring()
+#print(response1.read())     # can apply lxml.html.fromstring()
+
+#print("Headers:", response.info()) # headers
 
 for form in br.forms():
     print("Form name:", form.name)
-    print(form)
+    #print(form)
+    br.select_form(form.name) # form selection
+    #br.form['university_selector'] = 'mcmaster'
+    #br.submit(id='course_add_button')
+    print("End of form\n")
+
+print("Finished")
+
+
